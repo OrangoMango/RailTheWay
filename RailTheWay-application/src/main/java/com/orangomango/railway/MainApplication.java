ece42525de9754e4c9c1420d954e9d035dbec4c7
OrangoMango <orapama06@gmail.com>
2023-08-05 08:15:00 +0200

    changed size

--- RailTheWay-application/src/main/java/com/orangomango/railway/MainApplication.java
+++ RailTheWay-application/src/main/java/com/orangomango/railway/MainApplication.java
@@ -2,6 +2,7 @@ package com.orangomango.railway;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.Screen;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
@@ -10,9 +11,9 @@ import com.orangomango.railway.ui.HomeScreen;
 import dev.webfx.platform.resource.Resource;
 
 public class MainApplication extends Application{
-	private static final int WIDTH = 575; //1150;
-	private static final int HEIGHT = 375; //750;
-	private static final double SCALE = 0.5; //1;
+	private static final int HEIGHT = (int)Screen.getPrimary().getVisualBounds().getHeight(); //750;
+	private static final int WIDTH = HEIGHT*0.65; //1150;
+	private static final double SCALE = WIDTH/1150; //1;
 	private static final int FPS = 40;
 	public static Stage stage;
 	private static final Media BACKGROUND_MUSIC = new Media(Resource.toUrl("/audio/background.mp3", MainApplication.class));
